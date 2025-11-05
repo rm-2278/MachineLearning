886b448627587f265060c818dd64c8e236635d0e
kabi73 <rikimys73@gmail.com>
2025-11-05 23:36:49 +0000

    Created the base.

---
+++ NeuralNetworkOptimization/README.md
@@ -0,0 +1,2 @@
+# Neural Network with optimization
+Implements neural network with Pytorch. Uses regularizers and optimizers to improve the performance.
---
+++ NeuralNetworkOptimization/model.py
@@ -0,0 +1,16 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+
+class MLP(nn.Module):
+    def __init__(self, in_dim, hid_dim, out_dim):
+        super().__init__()
+        self.fc1 = nn.Linear(in_dim, hid_dim)
+        self.fc2 = nn.Linear(hid_dim, out_dim)
+        
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.softmax(self.fc2(x), dim=1)
